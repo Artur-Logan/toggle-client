7f3d1057c5a705dee39bbab2124f601446a693ad
Artur Logan <arturlogan1ferreira@gmail.com>
2025-01-29 13:20:16 -0300

    Primeiro commit

---
+++ toggle-client/src/main/java/com/arturlogan/toggle_client_ebac/ToggleClientEbacApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.toggle_client_ebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToggleClientEbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToggleClientEbacApplication.class, args);
+	}
+
+}
---
+++ toggle-client/src/test/java/com/arturlogan/toggle_client_ebac/ToggleClientEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.toggle_client_ebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToggleClientEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
